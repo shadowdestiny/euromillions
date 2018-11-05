cf7ae3f803c9a2ed650c8d5135379d848b056e9e
Luis Marin <lrm.programador@gmail.com>
2018-11-05 02:20:44 -0400

    Configurando cors

--- app/Http/Controllers/EuromillionsDrawController.php
+++ app/Http/Controllers/EuromillionsDrawController.php
@@ -19,14 +19,35 @@ class EuromillionsDrawController extends Controller
     }
 
     public function getResult(Request $request){
-        if (/*$request->isJson()*/ true) {
+        if ($request->isJson()) {
 
             $euromillions = Cache::remember('usersTable', 1, function() {
                 return EuromillionsDraw::all()->first();
             });
 
             if ($euromillions){
-                return response()->json($euromillions, 201);
+                //return response()->json($euromillions, 201);
+
+                $results = [
+                    $euromillions->result_regular_number_one,
+                    $euromillions->result_regular_number_two,
+                    $euromillions->result_regular_number_three,
+                    $euromillions->result_regular_number_four,
+                    $euromillions->result_regular_number_five,
+                    $euromillions->result_lucky_number_one,
+                    $euromillions->result_lucky_number_two,
+                ];
+
+                $result = implode(",",$results);
+
+                $data = [
+                    "error"     => 0,
+                    "draw"      => $euromillions->draw_date,
+                    "results"   => $result
+                ];
+
+                return response()->json($data, 201);
+
             } else {
                 return response()->json(['error' => 'User found'], 401, []);
             }
